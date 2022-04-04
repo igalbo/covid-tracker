54456b7a98c9cd01eb5de1f3573e676b3d903d9f
Igal <igalbog@gmail.com>
2022-04-04 18:10:42 +0300

    Updates

---
+++ precache-manifest.b1bd9cccef8e9888c4bb0eaf5ddd056d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4774dc1938f341e31f8e08d3079b06a4",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "316bf02196122bb036a5",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b666fca8e0f40f95a3ae",
+    "url": "/covid-tracker/static/css/main.ef28879d.chunk.css"
+  },
+  {
+    "revision": "316bf02196122bb036a5",
+    "url": "/covid-tracker/static/js/2.88702f01.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-tracker/static/js/2.88702f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b666fca8e0f40f95a3ae",
+    "url": "/covid-tracker/static/js/main.3413037c.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
